40dca0462612ccf4317dbcfbcb358a8456865c07
Iain Soars <iain@schweben.org>
2024-04-24 08:58:28 +0100

    Tidied test class

--- src/test/java/org/schweben/dinghyapi/repositories/DinghyRepositoryTest.java
+++ src/test/java/org/schweben/dinghyapi/repositories/DinghyRepositoryTest.java
@@ -2,9 +2,6 @@ package org.schweben.dinghyapi.repositories;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.schweben.dinghyapi.entities.Dinghy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
