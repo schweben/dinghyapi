0995870d89731cd1348785d05c546e8c3968a1c9
Iain Soars <iain@schweben.org>
2024-04-28 15:31:51 +0100

    Better handling of spinnakers

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -2,7 +2,7 @@ export default function Results(props) {
     return (
         <div>
         { props.dinghies.map((dinghy) =>
-        <div key="{dinghy.id}">
+        <div key={dinghy.id}>
             <h3>{dinghy.name}</h3>
             <ul>
                 <li>LOA: {dinghy.length}m</li>
@@ -12,10 +12,16 @@ export default function Results(props) {
                 <li>Rig: {dinghy.rig}</li>
                 <li>Crew: {dinghy.crew}</li>
                 <li>Sail area: {dinghy.sailArea}m<sup>2</sup></li>
-                <li>Symmetric spinnaker: {dinghy.symmetricSpinnaker}</li>
-                <li>Asymmetric spinnaker: {dinghy.asymmetricSpinnaker}</li>
-                <li>Spinnaker area: {dinghy.spinnakerArea}m<sup>2</sup></li>
-                <li>Trapeze: {dinghy.trapeze}</li>
+                {dinghy.symmetricSpinnaker || dinghy.asymmetricSpinnaker ? (
+                    <div>
+                        <li>Spinnaker:
+                            <input type="checkbox" disabled="true" defaultChecked={dinghy.symmetricSpinnaker ? (true) : (false)} />Symmetric
+                            <input type="checkbox" disabled="true" defaultChecked={dinghy.asymmetricSpinnaker ? (true) : (false)} />Asymmetric
+                        </li>
+                        <li>Spinnaker area: {dinghy.spinnakerArea}m<sup>2</sup></li>
+                    </div>
+                ) : null}
+                {dinghy.trapeze > 0 ? (<li>Trapeze: {dinghy.trapeze}</li>) : null}
                 <li>Manufacturer: {dinghy.manufacturer}</li>
             </ul>
         </div>
