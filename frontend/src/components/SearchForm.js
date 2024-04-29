d000ee9b68ad7d5cb0c4e120551b8e964cd07c6e
Iain Soars <iain@schweben.org>
2024-04-29 15:44:09 +0100

    Improved format of search form

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -9,34 +9,48 @@ export default function SearchForm(props) {
         <form className={styles.container} onSubmit={handleSubmit((data) => {
             props.search(data.name, data.manufacturer, data.crew, data.symmetric, data.asymmetric, data.trapeze);
     })}>
+        <div>
             <label htmlFor="name">Class name:</label>
             <input type="text"
                 name="name"
                 placeholder="Class name"
                 {...register("name")}/>
+        </div>
+        <div>
             <label htmlFor="manufacturer">Manufacturer:</label>
             <input type="text"
                 name="manufacturer"
                 placeholder="Manufacturer"
                 {...register("manufacturer")}/>
+        </div>
+        <div>
             <label htmlFor="crew">Number of crew:</label>
             <input type="number"
                 name="crew"
                 placeholder="Crew"
                 {...register("crew")}/>
-            <label htmlFor="symmetric">Symmetric spinnaker:</label>
+        </div>
+        <div>
             <input type="checkbox"
                 name="symmetric"
                 {...register("symmetric")} />
-            <label htmlFor="asymmetric">Asymmetric spinnaker:</label>
+            <label className={styles.checkboxText} htmlFor="symmetric">Symmetric spinnaker</label>
+        </div>
+        <div>
             <input type="checkbox"
                 name="asymmetric"
                 {...register("asymmetric")} />
-            <label htmlFor="trapeze">Trapeze:</label>
+            <label className={styles.checkboxText} l htmlFor="asymmetric">Asymmetric spinnaker</label>
+        </div>
+        <div>
             <input type="checkbox"
                 name="trapeze"
                 {...register("trapeze")}/>
+            <label className={styles.checkboxText} htmlFor="trapeze">Trapeze</label>
+        </div>
+        <div>
             <input type="submit" value="Search"/>
+        </div>
         </form>
     )
 }
