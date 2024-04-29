3031183dd2f5df0645e9abdd5b4bd82349931c54
Iain Soars <iain@schweben.org>
2024-04-29 17:16:14 +0100

    Improved results formatting

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -4,8 +4,8 @@ export default function Results(props) {
     return (
         <div>
         { props.dinghies?.map((dinghy) =>
-        <div key={dinghy.id}>
-            <h3>{dinghy.name}</h3>
+        <div className={styles.result} key={dinghy.id}>
+            <h2>{dinghy.name}</h2>
             <ul>
                 <li>LOA: {dinghy.length}m</li>
                 <li>Beam: {dinghy.beam}m</li>
@@ -15,16 +15,18 @@ export default function Results(props) {
                 <li>Crew: {dinghy.crew}</li>
                 <li>Sail area: {dinghy.sailArea}m<sup>2</sup></li>
                 {dinghy.symmetricSpinnaker || dinghy.asymmetricSpinnaker ? (
-                    <div>
-                        <li>Spinnaker:
-                            <input type="checkbox" disabled="true" defaultChecked={dinghy.symmetricSpinnaker ? (true) : (false)} />Symmetric
-                            <input type="checkbox" disabled="true" defaultChecked={dinghy.asymmetricSpinnaker ? (true) : (false)} />Asymmetric
-                        </li>
-                        <li>Spinnaker area: {dinghy.spinnakerArea}m<sup>2</sup></li>
-                    </div>
+                    <li>Spinnaker:
+                        <input type="checkbox" disabled="true" defaultChecked={dinghy.symmetricSpinnaker ? (true) : (false)} />Symmetric
+                        <input type="checkbox" disabled="true" defaultChecked={dinghy.asymmetricSpinnaker ? (true) : (false)} />Asymmetric
+                    </li>
+                ) : null}
+                {dinghy.symmetricSpinnaker || dinghy.asymmetricSpinnaker ? (
+                    <li>Spinnaker area: {dinghy.spinnakerArea}m<sup>2</sup></li>
                 ) : null}
                 {dinghy.trapeze > 0 ? (<li>Trapeze: {dinghy.trapeze}</li>) : null}
-                <li>Manufacturer: {dinghy.manufacturer}</li>
+                {dinghy.manufacturer ? (
+                    <li>Manufacturer: {dinghy.manufacturer}</li>
+                ) : null}
             </ul>
         </div>
         )}
