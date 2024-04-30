00ce3e5b059ed37d55ea6e75b6ac321346759bde
Iain Soars <iain@schweben.org>
2024-04-30 14:52:42 +0100

    Added image as background

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -5,29 +5,32 @@ export default function Results(props) {
         <div>
         { props.dinghies?.map((dinghy) =>
         <div className={styles.result} key={dinghy.id}>
-            <h2>{dinghy.name}</h2>
-            <ul>
-                <li><span className={styles.label}>LOA:</span> {dinghy.length}m</li>
-                    <li><span className={styles.label}>Beam:</span> {dinghy.beam}m</li>
-                    <li><span className={styles.label}>Weight</span>: {dinghy.hullWeight}kg</li>
-                    <li><span className={styles.label}>PY Number:</span> {dinghy.yardstick}</li>
-                    <li><span className={styles.label}>Rig:</span> {dinghy.rig}</li>
-                    <li><span className={styles.label}>Crew:</span> {dinghy.crew}</li>
-                    <li><span className={styles.label}>Sail area:</span> {dinghy.sailArea}m<sup>2</sup></li>
-                {dinghy.symmetricSpinnaker || dinghy.asymmetricSpinnaker ? (
-                        <li><span className={styles.label}>Spinnaker:</span>
-                        <input type="checkbox" disabled="true" defaultChecked={dinghy.symmetricSpinnaker ? (true) : (false)} />Symmetric
-                        <input type="checkbox" disabled="true" defaultChecked={dinghy.asymmetricSpinnaker ? (true) : (false)} />Asymmetric
-                    </li>
-                ) : null}
-                {dinghy.symmetricSpinnaker || dinghy.asymmetricSpinnaker ? (
-                        <li><span className={styles.label}>Spinnaker area:</span> {dinghy.spinnakerArea}m<sup>2</sup></li>
-                ) : null}
-                {dinghy.trapeze > 0 ? (<li><span className={styles.label}>Trapeze:</span> {dinghy.trapeze}</li>) : null}
-                {dinghy.manufacturer ? (
-                    <li><span className={styles.label}>Manufacturer:</span> {dinghy.manufacturer}</li>
-                ) : null}
-            </ul>
+            <img className={styles.resultBackground} src="http://localhost:8080/images/tera-pro.jpeg" alt={dinghy.name}/>
+            <div className={styles.resultContent}>
+                <h2>{dinghy.name}</h2>
+                <ul>
+                    <li><span className={styles.label}>LOA:</span> {dinghy.length}m</li>
+                        <li><span className={styles.label}>Beam:</span> {dinghy.beam}m</li>
+                        <li><span className={styles.label}>Weight</span>: {dinghy.hullWeight}kg</li>
+                        <li><span className={styles.label}>PY Number:</span> {dinghy.yardstick}</li>
+                        <li><span className={styles.label}>Rig:</span> {dinghy.rig}</li>
+                        <li><span className={styles.label}>Crew:</span> {dinghy.crew}</li>
+                        <li><span className={styles.label}>Sail area:</span> {dinghy.sailArea}m<sup>2</sup></li>
+                    {dinghy.symmetricSpinnaker || dinghy.asymmetricSpinnaker ? (
+                            <li><span className={styles.label}>Spinnaker:</span>
+                            <input type="checkbox" disabled="true" defaultChecked={dinghy.symmetricSpinnaker ? (true) : (false)} />Symmetric
+                            <input type="checkbox" disabled="true" defaultChecked={dinghy.asymmetricSpinnaker ? (true) : (false)} />Asymmetric
+                        </li>
+                    ) : null}
+                    {dinghy.symmetricSpinnaker || dinghy.asymmetricSpinnaker ? (
+                            <li><span className={styles.label}>Spinnaker area:</span> {dinghy.spinnakerArea}m<sup>2</sup></li>
+                    ) : null}
+                    {dinghy.trapeze > 0 ? (<li><span className={styles.label}>Trapeze:</span> {dinghy.trapeze}</li>) : null}
+                    {dinghy.manufacturer ? (
+                        <li><span className={styles.label}>Manufacturer:</span> {dinghy.manufacturer}</li>
+                    ) : null}
+                </ul>
+            </div>
         </div>
         )}
         </div>
