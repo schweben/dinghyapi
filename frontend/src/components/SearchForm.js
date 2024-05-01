ee1f70c9d1c313c70609f1a27245514ce304bf14
Iain Soars <iain@schweben.org>
2024-05-01 15:47:19 +0100

    Improved search form layout

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -9,49 +9,55 @@ export default function SearchForm(props) {
         <form className={styles.container} onSubmit={handleSubmit((data) => {
             props.search(data.name, data.manufacturer, data.crew, data.symmetric, data.asymmetric, data.trapeze);
     })}>
-        <div>
-            <label htmlFor="name">Class name:</label>
-            <input type="text"
-                name="name"
-                placeholder="Class name"
-                {...register("name")}/>
-        </div>
-        <div>
-            <label htmlFor="manufacturer">Manufacturer:</label>
-            <input type="text"
-                name="manufacturer"
-                placeholder="Manufacturer"
-                {...register("manufacturer")}/>
-        </div>
-        <div>
-            <label htmlFor="crew">Number of crew:</label>
-            <input type="number"
-                name="crew"
-                placeholder="Crew"
-                {...register("crew")}/>
-        </div>
-        <div>
-            <input type="checkbox"
-                name="symmetric"
-                {...register("symmetric")} />
-            <label className={styles.checkboxText} htmlFor="symmetric">Symmetric spinnaker</label>
-        </div>
-        <div>
-            <input type="checkbox"
-                name="asymmetric"
-                {...register("asymmetric")} />
-            <label className={styles.checkboxText} l htmlFor="asymmetric">Asymmetric spinnaker</label>
-        </div>
-        <div>
-            <input type="checkbox"
-                name="trapeze"
-                {...register("trapeze")}/>
-            <label className={styles.checkboxText} htmlFor="trapeze">Trapeze</label>
-        </div>
-        <div>
-            <input type="submit" value="Search"/>
-            <input type="button" value="Clear" onClick={props.clearResults}/>
-        </div>
+            <div className={styles.formRow}>
+                <div className={styles.formElement}>
+                    <label htmlFor="name">Class name:</label>
+                    <input type="text"
+                        name="name"
+                        placeholder="Class name"
+                        {...register("name")}/>
+                </div>
+                    <div className={styles.formElement}>
+                    <label htmlFor="manufacturer">Manufacturer:</label>
+                    <input type="text"
+                        name="manufacturer"
+                        placeholder="Manufacturer"
+                        {...register("manufacturer")}/>
+                </div>
+                    <div className={styles.formElement}>
+                    <label htmlFor="crew">Number of crew:</label>
+                    <input type="number"
+                        name="crew"
+                        placeholder="Crew"
+                        {...register("crew")}/>
+                </div>
+            </div>
+            <div className={styles.formRow}>
+                <div className={styles.formElement}>
+                    <input type="checkbox"
+                        name="symmetric"
+                        {...register("symmetric")} />
+                    <label className={styles.checkboxText} htmlFor="symmetric">Symmetric spinnaker</label>
+                </div>
+                    <div className={styles.formElement}>
+                    <input type="checkbox"
+                        name="asymmetric"
+                        {...register("asymmetric")} />
+                    <label className={styles.checkboxText} l htmlFor="asymmetric">Asymmetric spinnaker</label>
+                </div>
+                    <div className={styles.formElement}>
+                    <input type="checkbox"
+                        name="trapeze"
+                        {...register("trapeze")}/>
+                    <label className={styles.checkboxText} htmlFor="trapeze">Trapeze</label>
+                </div>
+            </div>
+            <div className={styles.formRow}>
+                <div className={styles.formElement}>
+                    <input type="submit" value="Search"/>
+                    <input type="button" value="Clear" onClick={props.clearResults}/>
+                </div>
+            </div>
         </form>
     )
 }
